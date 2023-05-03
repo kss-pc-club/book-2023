b0e02f4eb790990733d7cfc0b33a9e713f21913e
eno1220 <eno1220dev@gmail.com>
2023-05-04 00:45:46 +0900

    update

--- vivliostyle.config.js
+++ vivliostyle.config.js
@@ -1,28 +1,21 @@
 module.exports = {
-  title: "KSS PC Book 2023", // populated into `publication.json`, default to `title` of the first entry or `name` in `package.json`.
-  author: "kss-pc-club", // default to `author` in `package.json` or undefined.
-  language: "ja", // default to undefined.
-  size: "JIS-B5", // paper size.
-  theme: "css/theme_print.css", // .css or local dir or npm package. default to undefined.
+  title: "KSS PC Book 2023", 
+  author: "kss-pc-club",
+  language: "ja",
+  size: "JIS-B5",
+  theme: "css/theme_print.css",
   entry: [
     "index.md",
-    "comments.md", // `title` is automatically guessed from the file (frontmatter > first heading).
-    "questionnaire/index.md", // path can be absolute or relative to `entryContext`.
+    "comments.md",
+    "questionnaire/index.md",
     "covid19-ibaraki/index.md",
     "colophon.md"
-    // {
-    //   path: 'epigraph.md',
-    //   title: 'Epigraph', // title can be overwritten (entry > file),
-    //   theme: '@vivliostyle/theme-whatever', // theme can be set individually. default to the root `theme`.
-    // },
-    // 'glossary.html', // html can be passed.
-  ], // `entry` can be `string` or `object` if there's only single markdown file.
-  entryContext: "./manuscripts", // default to '.' (relative to `vivliostyle.config.js`).
-  output: [
-    // path to generate draft file(s). default to '{title}.pdf'
-    "./public/book.pdf", // the output format will be inferred from the name.
   ],
-  workspaceDir: ".cache", // directory which is saved intermediate files.
+  entryContext: "./manuscripts",
+  output: [
+    "./public/book.pdf", 
+  ],
+  workspaceDir: ".cache",
   // toc: true, // whether generate and include ToC HTML or not, default to 'false'.
   // cover: './cover.png', // cover image. default to undefined.
   // vfm: { // options of VFM processor
