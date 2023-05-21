c7ecd9b5fba2f65804c596b66be9e109fa60c20b
eno1220 <86103453+eno1220@users.noreply.github.com>
2023-05-21 16:41:15 +0900

    eno1220 (#17)
    * update: 記事の順番を変更
    
    * fix: eno1220の記事のtypoを修正
    
    * update: 執筆者紹介を追加
    
    * fix: typo
    
    * update: 執筆者紹介を追加
    
    * update: 目次のレイアウトを変更
    
    * fix: 「はじめに」を修正
    
    * fix: 「部員アンケート2023」を修正

--- manuscripts/index.md
+++ manuscripts/index.md
@@ -15,6 +15,17 @@ title: 目次
 
 # 目次 {.doc}
 
+- [はじめに](foreword.html)
+- [第 1 章 部員アンケート 2023](questionnaire/index.html)
+- [第 2 章 茨城県版新型コロナ対策サイトの振り返り](covid19-ibaraki/index.html)
+- [第 3 章 ﾊﾟｿｺﾝｶﾀｶﾀしてたら大学合格した話 + 大学生活所感](tsukuba-ac/index.html)
+- [第 4 章 学生のキャッシュレス決済を考える](cashless-payment/index.html)
+- [第 5 章 ソフトウェアシンセサイザを作ってみよう](Synthesizer/index.html)
+- [第 6 章 ニコニコ動画風コメント掲示板２はこうやって作られた](hrkn63_hnm/index.html)
+- [第 7 章 茨城県教育情報ネットワークの脆弱性を報告した話](eno1220/index.html)
+- [第 8 章 技術と人間 + pygameについて](tech-and-human/index.html)
+- [第 9 章 AIと一緒にゲームを作ろう！](make-game-with-ai/index.html)
+- [第 10 章 「文字の自動生成の仕組み」について](font-generation/index.html)
 - [執筆者紹介](comments.html)
 
 </nav>
--- vivliostyle.config.js
+++ vivliostyle.config.js
@@ -1,29 +1,27 @@
 module.exports = {
-  title: "KSS PC Book 2023", 
+  title: "KSS PC Book 2023",
   author: "kss-pc-club",
   language: "ja",
   size: "JIS-B5",
   theme: "css/theme_print.css",
   entry: [
     "index.md",
-    "comments.md",
+    "foreword.md",
     "questionnaire/index.md",
     "covid19-ibaraki/index.md",
     "tsukuba-ac/index.md",
+    "cashless-payment/index.md",
+    "Synthesizer/index.md",
+    "hrkn63_hnm/index.md",
+    "eno1220/index.md",
     "tech-and-human/index.md",
     "make-game-with-ai/index.md",
     "font-generation/index.md",
-    "eno1220/index.md",
-    "cashless-payment/index.md",
-    "Synthesizer/index.md",
-    "foreword.md/index.md",
-    "foreword.md",
-    "colophon.md"
+    "comments.md",
+    "colophon.md",
   ],
   entryContext: "./manuscripts",
-  output: [
-    "./public/book.pdf", 
-  ],
+  output: ["./public/book.pdf"],
   workspaceDir: ".cache",
   // toc: true, // whether generate and include ToC HTML or not, default to 'false'.
   // cover: './cover.png', // cover image. default to undefined.
